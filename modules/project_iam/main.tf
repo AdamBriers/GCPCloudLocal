68b021decdd3f28f7828305f3c03f4ccfbf04029
Mike Owen <mike.owen@b2binternetsolutions.co.uk>
2021-01-31 11:16:59 +0000

    Enhanced project_iam module so it needs to be called once only per project

--- modules/project_iam/main.tf
+++ modules/project_iam/main.tf
@@ -1,11 +1,28 @@
+
 locals {
-  member_id = var.member_type == "user" ? "user:${var.member_name}" : var.member_type == "serviceaccount" ? "serviceAccount:${var.member_name}" : var.member_type == "group" ? "group:${var.member_name}" : "domain:${var.member_name}"
+  project_member_permissions = flatten([
+
+    for m, member in var.project_members : [
+      
+      for role, permission in member.project_iam_permissions : {
+        project = var.project_id
+        role    = permission
+        member  = lookup(member, "member_type", "WARNING-MEMBER-TYPE-NOT-FOUND") == "user" ? "user:${lookup(member, "member_name", "WARNING-MEMBER-TYPE-NOT-FOUND")}" : lookup(member, "member_type", "WARNING-MEMBER-TYPE-NOT-FOUND") == "serviceaccount" ? "serviceAccount:${lookup(member, "member_name", "WARNING-MEMBER-TYPE-NOT-FOUND")}" : lookup(member, "member_type", "WARNING-MEMBER-TYPE-NOT-FOUND") == "group" ? "group:${lookup(member, "member_name", "WARNING-MEMBER-TYPE-NOT-FOUND")}" : "domain:${lookup(member, "member_name", "WARNING-MEMBER-TYPE-NOT-FOUND")}"
+      }
+    ]
+  ])
 }
 
+/******************************************
+	Member Permissions
+ *****************************************/
 resource "google_project_iam_member" "this" {
-  for_each = toset(var.project_iam_permissions)
-
-  project = var.project_id
-  role   = each.value
-  member = local.member_id
+  for_each = {
+    # Generate a unique string identifier for each role
+    for mr in local.project_member_permissions : "${mr.member}-${mr.role}" => mr
+  }   
+  
+  project = each.value.project
+  member  = each.value.member
+  role    = each.value.role
 }
