ac0e2fc60d5f13597226429cfa88738c21ced237
Mike Owen <mike.owen@b2binternetsolutions.co.uk>
2021-02-02 10:50:18 +0000

    Corrected bug

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -4,6 +4,6 @@ output "subnets" {
 }
 
 output "subnet_ids" {
-  value       = valuse(google_compute_subnetwork.sub_network)[*].id
+  value       = values(google_compute_subnetwork.sub_network)[*].id
   description = "The created subnets resources"
 }
