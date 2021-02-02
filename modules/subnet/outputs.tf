bc45bc70aa7c52368e45796812a7d0d11af113ee
Mike Owen <mike.owen@b2binternetsolutions.co.uk>
2021-02-02 10:24:21 +0000

    Needed to add the subnet_ids output separately from the folder policies code in order for mjo-folder-policies branch to plan

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -3,23 +3,7 @@ output "subnets" {
   description = "The created subnets resources"
 }
 
-
-#output "sub_network_id" {
-#  value       = google_compute_subnetwork.sub_network.id
-#  description = "The subnetwork resource being created"
-#}
-
-#output "creation_timestamp" {
-#  value       = google_compute_subnetwork.sub_network.creation_timestamp
-#  description = "Creation timestamp in RFC3339 text format"
-#}
-
-#output "gateway_address" {
-#  value       = google_compute_subnetwork.sub_network.gateway_address
-#  description = "The gateway address for default routes to reach destination addresses outside this subnetwork"
-#}
-
-#output "sub_network_self_link" {
-#  value       = google_compute_subnetwork.sub_network.self_link
-#  description = "The URI of the subnetwork being created"
-#}
+output "subnet_ids" {
+  value       = valuse(google_compute_subnetwork.sub_network)[*].id
+  description = "The created subnets resources"
+}
