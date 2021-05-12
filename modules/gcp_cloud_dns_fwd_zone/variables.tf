3127efc617a7bd6be93fdeaa7a24ef035ac0e20d
theodorvaleanu-appsbroker <theodor.valeanu@appsbroker.com>
2021-05-12 17:06:40 +0300

    variable change

--- modules/gcp_cloud_dns_fwd_zone/variables.tf
+++ modules/gcp_cloud_dns_fwd_zone/variables.tf
@@ -1,46 +1,46 @@
-variable "description" {
-  description = "A description for the deployed DNS zone."
-  type        = string
-  default     = ""
-}
-
-variable "domain" {
-  description = "The DNS name for the deployed Cloud DNS zone."
-  type        = string
-}
-
-variable "name" {
-  description = "The 'user assigned' name for the deployed DNS Zone."
-  type        = string
-}
-
-variable "private_visibility_config_networks" {
-  description = "List of VPC self links that can see this zone."
-  default     = []
-  type        = list(string)
-}
+#variable "description" {
+#  description = "A description for the deployed DNS zone."
+#  type        = string
+#  default     = ""
+#}
+#
+#variable "domain" {
+#  description = "The DNS name for the deployed Cloud DNS zone."
+#  type        = string
+#}
+#
+#variable "name" {
+#  description = "The 'user assigned' name for the deployed DNS Zone."
+#  type        = string
+#}
+#
+#variable "private_visibility_config_networks" {
+#  description = "List of VPC self links that can see this zone."
+#  default     = []
+#  type        = list(string)
+#}
 
 variable "project_id" {
   description = "The ID of the project to deploy into."
   type        = string
 }
 
-variable "target_name_server_addresses" {
-  description = "List of target name servers for forwarding zone."
-  default     = []
-  type        = list(string)
-}
-
-variable "dnssec_config" {
-  description = "Object containing : kind, non_existence, state. Please see https://www.terraform.io/docs/providers/google/r/dns_managed_zone.html#dnssec_config for futhers details"
-  type        = any
-  default     = {}
-}
-
-variable "type" {
-  type        = string
-  description = "description"
-}
+#variable "target_name_server_addresses" {
+#  description = "List of target name servers for forwarding zone."
+#  default     = []
+#  type        = list(string)
+#}
+#
+#variable "dnssec_config" {
+#  description = "Object containing : kind, non_existence, state. Please see https://www.terraform.io/docs/providers/google/r/dns_managed_zone.html#dnssec_config for futhers details"
+#  type        = any
+#  default     = {}
+#}
+#
+#variable "type" {
+#  type        = string
+#  description = "description"
+#}
 
 variable peering_zones {
   default     = {}
