f5c03360c1e218f4200c96a3558880496a5f8911
theodorvaleanu-appsbroker <theodor.valeanu@appsbroker.com>
2021-05-12 16:44:42 +0300

    output change

--- modules/gcp_cloud_dns_fwd_zone/outputs.tf
+++ modules/gcp_cloud_dns_fwd_zone/outputs.tf
@@ -1,4 +1,9 @@
-output "zone_name" {
+output "peering_zone_name" {
   description = "The ID of the deployed DNS Zone."
-  value       = module.dns_zone.name
+  value       = module.dns_zone_peering.name
+}
+
+output "forwarding_zone_name" {
+  description = "The ID of the deployed DNS Zone."
+  value       = module.dns_forwarding_zone.name
 }
